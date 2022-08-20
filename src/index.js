8383150a0cc93169d68a19189c1597cb3935e12a
hdatdoo <dat.dh@omegatheme.com>
2022-08-20 16:11:02 +0700

    Router restructor

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ const path = require('path')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true,
+}))
+app.use(express.json())
 app.use(morgan('combined'))
-console.log(__dirname)
 app.engine('hbs', handlebars.engine(
   {extname: '.hbs'}
 ))
 app.set('view engine' , 'hbs')
 app.set('views' , path.join(__dirname, 'resources/views'))
-app.get('/', (req, res) => {
-  res.render('home')
-})
+
+route(app)
 
 app.listen(3000)
